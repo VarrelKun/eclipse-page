5e59ee63380037ccdeb6bc159315120e37930fcd
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-01-29 02:46:40 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/tos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'tos.html'));
+});
+
+app.get('/download', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dl.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
