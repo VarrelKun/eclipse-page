109ac7eccca9f82855836cf3782760fcdc41b6d5
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-01-29 11:15:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.get('/tos', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'tos.html'));
 });
-
 app.get('/download', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dl.html'));
+});
+app.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
+});
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'robots.txt'));
 });
 
 // Start server
